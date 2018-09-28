a1d4429cd6778affad33d58a6787f708a4a7850d
Shay <huybq.b8072@st.usth.edu.vn>
2018-09-29 01:38:09 +0700

    upload

--- js/show.js
+++ js/show.js
@@ -1,8 +1,5 @@
-function show(x){
-	//document.writeln(x);
-	//window.open(x);
-	document.getElementById('frame').src = x;
-	var e = document.getElementById('frame').style.display;
-	if ( e === "block") document.getElementById('frame').style.display = "none";
-	else document.getElementById('frame').style.display = "block";
+
+function show(x) {
+	
+    window.open(x);
 }
