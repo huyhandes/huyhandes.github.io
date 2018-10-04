f121037b62eb63d4aa176f6adbf2901c797655b2
Shay <huybq.b8072@st.usth.edu.vn>
2018-10-05 00:00:26 +0700

    upload

--- js/show.js
+++ js/show.js
@@ -3,8 +3,8 @@ function show(x) {
     window.open('../Code/' + x);
 }
 function reveal(x){
-	 if(document.getElementById(x).style.display === 'block') document.getElementById(x).style.display = 'none';
-	 else document.getElementById(x).style.display = 'block';
-	 if(x === 'stack') document.getElementById('queue').style.display = 'none';
-	 else	document.getElementById('stack').style.display = 'none';
+	if(document.getElementById(x).style.display === 'block') document.getElementById(x).style.display = 'none';
+	else document.getElementById(x).style.display = 'block';
+	if(x === 'stack') document.getElementById('queue').style.display = 'none';
+	else document.getElementById('stack').style.display = 'none';
 }
